1d7ba7410cccd93233e8e1914ae93e26a1fb5ce3
David <david.mccart@guestline.com>
2023-03-01 10:52:24 +0000

    update to latest use eslint not tslint

--- test/options.ts
+++ test/options.ts
@@ -1,25 +1,25 @@
 import test from 'ava';
 import { createClient } from '../src';
 
-test('throws if you do not pass the options object', async t => {
+test('throws if you do not pass the options object', async (t) => {
   await t.throwsAsync(async () => {
     await (createClient as any)();
   });
 });
 
-test('throws if you do not pass the urls parameter', async t => {
+test('throws if you do not pass the urls parameter', async (t) => {
   await t.throwsAsync(async () => {
     await createClient({} as any);
   });
 });
 
-test('throws if the urls parameter is empty', async t => {
+test('throws if the urls parameter is empty', async (t) => {
   await t.throwsAsync(async () => {
     await createClient({ urls: {} });
   });
 });
 
-test('throws if a url passed is not a valid uri specifying the url passed', async t => {
+test('throws if a url passed is not a valid uri specifying the url passed', async (t) => {
   await t.throwsAsync(
     async () => {
       await createClient({
@@ -33,7 +33,7 @@ test('throws if a url passed is not a valid uri specifying the url passed', asyn
   );
 });
 
-test('throws if the client parameter is not a function', async t => {
+test('throws if the client parameter is not a function', async (t) => {
   await t.throwsAsync(async () => {
     await createClient({
       urls: {
